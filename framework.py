1acdade9ffab159efdb2786ef6229eaf66238307
Philip Boeken <philip.boeken@hotmail.nl>
2017-03-14 23:11:51 +0000

    add framework

---
+++ framework.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#Models:
+    #Strategy
+    #Asset
+    #Portfolio
+    #Order
+
+#Execution:
+    #API connection
+    #Parse relevant data
+    #Analyse relevant data by strategy
+    #Decide action
+    #Execute action
+    #(Test fallback)
+
+#Support:
+    #Log
